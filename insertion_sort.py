c4a39ddf8a64be34a3f12b7286a2717174d6cb05
dellyjifferson <dellyjifferson509@gmail.com>
2026-01-04 21:17:51 -0500

    Insertion sort using swap and shift methods with comparison

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,5 @@
-def insertion_sort(arr):
+# In this program we implement the insertion sort algorithm to sort a list of even numbers.
+def insertion_sort_swap(arr):
     to_sort_length = range(1, len(arr))
     for i in to_sort_length:
         value_to_sort = arr[i]
@@ -8,3 +9,27 @@ def insertion_sort(arr):
             i -= 1
 
     return arr
+
+# Testing the function to sort a list of even numbers
+even_numbers = [28, 4, 2, 6, 8, 10, 14, 16, 12]
+sorted_numbers = insertion_sort_swap(even_numbers)
+print(sorted_numbers)
+
+# Insertion Sort Algorithm Shift method
+
+def insertion_sort_shift(arr):
+    for i in range(1, len(arr)):
+        value_to_sort = arr[i]
+        position = i
+
+        while position > 0 and arr[position - 1] > value_to_sort:
+            arr[position] = arr[position - 1]
+            position -= 1
+
+        arr[position] = value_to_sort
+    return arr
+
+# Testing the function to sort a list of odd numbers
+odd_numbers = [27, 3, 1, 5, 7, 9, 13, 15, 11]
+sorted_odd_numbers = insertion_sort_shift(odd_numbers)
+print(sorted_odd_numbers)
