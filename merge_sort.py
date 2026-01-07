7dd02cb10efef4ebd929f21989c909000effaad6
dellyjifferson <dellyjifferson509@gmail.com>
2026-01-06 20:29:27 -0500

    Implement Merge Sort algorithm in Python

---
+++ merge_sort.py
@@ -0,0 +1,35 @@
+# In this program we will implement the Merge Sort algorithm in Python.
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]        # Dividing the elements into 2 halves the left half
+        R = arr[mid:]        # The Right half
+
+        merge_sort(L)        # Sorting the first half
+        merge_sort(R)        # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    return arr
